9e7d8a9ba054281d0f938edd1f6b1695f3415848
Surani Rodrigo <suranirodrigo14402@gmail.com>
2026-04-25 14:47:37 +0530

    Configure WebSocket message broker and static resource handlers for file uploads

--- backend/src/main/java/com/project/config/WebConfig.java
+++ backend/src/main/java/com/project/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.project.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- backend/src/main/java/com/project/config/WebSocketConfig.java
+++ backend/src/main/java/com/project/config/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package com.project.config;
 
+import org.springframework.lang.NonNull;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -11,13 +13,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/queue"); // Use /topic for broadcast, /queue for point-to-point
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("http://localhost:5173")
                 .withSockJS();
